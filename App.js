7d9c678631ac5f735f2ac5388befbf1385ea7e8f
Lucas Sena <lucassena.rj@gmail.com>
2018-06-21 18:19:46 -0300

    Implement props on Header

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <View>
-        <Header />
+        <Header title="Header title" />
       </View>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-const Header = () => {
-    return <Text>Title</Text>
+const Header = (props) => {
+    return <Text>{props.title}</Text>
 }
 
 export default Header;
